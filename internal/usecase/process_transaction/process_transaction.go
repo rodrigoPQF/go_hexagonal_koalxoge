7f67808714af2e890c98d9218f303a06323d30f0
rodrigopr404 <rodrigopr404@gmail.com>
2023-08-08 03:23:12 -0300

    🚧 Create Usecase process transaction

---
+++ internal/usecase/process_transaction/process_transaction.go
@@ -0,0 +1,42 @@
+package process_transaction
+
+import "github.com/rodrigoPQF/go_hexagonal_koalxoge/internal/entity"
+
+type ProcessTransaction struct {
+	Repository entity.TransactionRepository
+}
+
+func NewProcessTransaction(repository entity.TransactionRepository) *ProcessTransaction {
+	return &ProcessTransaction{Repository: repository}
+}
+
+func (p *ProcessTransaction) Execute(input TransactionDtoInput) (TransactionDtoOutput, error) {
+	transaction := entity.NewTransaction()
+	transaction.ID = input.ID
+	transaction.AccountID = input.AccountID
+	transaction.Amount = input.Amount
+	invalidTransaction := transaction.IsValid()
+
+	if invalidTransaction != nil {
+		err := p.Repository.Insert(transaction.ID, transaction.AccountID, transaction.Amount, "rejected", invalidTransaction.Error())
+		if err != nil {
+			return TransactionDtoOutput{}, err
+		}
+		output := TransactionDtoOutput{
+			ID: transaction.ID,
+			Status: "rejected",
+			ErrorMessage: invalidTransaction.Error(),
+		}
+		return output, nil
+	}
+	err := p.Repository.Insert(transaction.ID, transaction.AccountID, transaction.Amount, "approved", "")
+	if err != nil {
+		return TransactionDtoOutput{}, err
+	}
+	output := TransactionDtoOutput{
+		ID: transaction.ID,
+		Status: "approved",
+		ErrorMessage: invalidTransaction.Error(),
+	}
+	return output, nil
+}
---
+++ internal/usecase/process_transaction/process_transaction_test.go
@@ -0,0 +1,37 @@
+package process_transaction
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	mock_entity "github.com/rodrigoPQF/go_hexagonal_koalxoge/internal/entity/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProcessTransactionWhenItsValid(t *testing.T) {
+	input := TransactionDtoInput {
+		ID: "1",
+		AccountID: "1",
+		Amount: 200,
+	}
+
+	expectedOutput := TransactionDtoOutput {
+		ID: "1",
+		Status: "approved",
+		ErrorMessage: "",
+	}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	
+	repositoryMock := mock_entity.NewMockTransactionRepository(ctrl)
+	repositoryMock.EXPECT().Insert(input.ID, input.AccountID, input.Amount, "approved", "").Return(nil)
+
+	usecase := NewProcessTransaction(repositoryMock)
+	output, err := usecase.Execute(input)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedOutput, output)
+	
+
+
+}
